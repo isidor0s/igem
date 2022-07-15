dcee9e6fd17d6de73221117328def52ae7ffd110
BabisDigos <85764745+BabisDigos@users.noreply.github.com>
2022-07-15 15:34:49 +0300

    Menu Dropdowns Desktop
    Theloune styling gt tr einai gtp

--- new/script.js
+++ new/script.js
@@ -1,6 +1,61 @@
 
-// const menu = document.querySelector('.navigation-menu');
+/* Navigation Menu Dropdowns */
+
+$('#project').click(function() {
+    project = document.getElementById('project-content');
+    closeMenuDropdownsExceptSelf(project);
+    project.classList.toggle('--show');
+})
+
+$('#labs').click(function() {
+    labs = document.getElementById('labs-content');
+    closeMenuDropdownsExceptSelf(labs);
+    labs.classList.toggle('--show');
+})
+
+$('#hp').click(function() {
+    hp = document.getElementById('hp-content');
+    closeMenuDropdownsExceptSelf(hp);
+    hp.classList.toggle('--show');
+})
+
+$('#team').click(function() {
+    team = document.getElementById('team-content');
+    closeMenuDropdownsExceptSelf(team);
+    team.classList.toggle('--show');
+})
+
+function closeMenuDropdowns() {
+    var dropdownLists = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdownLists.length; i++) {
+      if (dropdownLists[i].classList.contains('--show')) {
+        dropdownLists[i].classList.remove('--show');
+      }
+    }
+}
+
+function closeMenuDropdownsExceptSelf(self) {
+    var dropdownLists = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdownLists.length; i++) {
+      if (dropdownLists[i] != self) {
+        dropdownLists[i].classList.remove('--show');
+      }
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.navigation-menu .dropdown-button')) {
+        closeMenuDropdowns();
+    }
+}
+
+/* Navigation Menu Hamburger Button */
+
 const menuHamBtn = document.querySelector('.menu-toggle-button')
+
+menuHamBtn.addEventListener('click', handleClick);
 
 const handleClick = (e) => {
     const toggleMenu = () => menuHamBtn.classList.toggle('open');
@@ -19,6 +74,3 @@ const addOffClick = (e, cb) => {
     }
     document.addEventListener('click', offClick);
 }
-
-menuHamBtn.addEventListener('click', handleClick);
-
